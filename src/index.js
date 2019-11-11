fb09dd0de5fb24055ef4e41dddb5538987e339ac
Steven Bruno <sbruno636@gmail.com>
2019-11-10 21:31:43 -0600

    create gif search field

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import { getGif } from './getGif';
 
 const img = document.querySelector('#mainImg');
+const btn = document.querySelector('#searchBtn');
+const sField = document.querySelector('#searchField');
+btn.addEventListener('click', newSearch);
+
+function newSearch() {
+  const term = sField.value;
+  let newImg = getGif(term);
+  Promise.resolve(newImg).then((res) => img.src = res);
+}
+
 let imgSrc = getGif('dog');
-Promise.resolve(imgSrc).then((src) => img.src = src);
+Promise.resolve(imgSrc).then((src) => img.src = src); 
